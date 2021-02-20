00b07bea9a52a50d906049694ab6bc43b2470636
kmtym1998 <kmtym.devloper@gmail.com>
2021-02-21 01:44:57 +0900

    共通部分

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
-import '../styles/globals.css';
+import React, { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+import {
+  ThemeProvider as MaterialUIThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+import AppHeader from '../components/AppHeader';
+import AppFooter from '../components/AppFooter';
+import '../styles/globals.css';
+import theme from '../styles/theme';
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppHeader></AppHeader>
+          <Container maxWidth="sm">
+            <Component {...pageProps} />
+          </Container>
+          <AppFooter></AppFooter>
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  );
+};
 
 export default MyApp;
