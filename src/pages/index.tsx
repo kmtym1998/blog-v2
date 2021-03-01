60767f509bf510474cd0a8f699cd046e6daa5587
kmtym1998 <kmtym.devloper@gmail.com>
2021-03-01 23:51:54 +0900

    [WIP] 写真のグリッド表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import styles from '../styles/index.module.css';
 import Icons from '../components/Icons';
 
@@ -11,37 +15,43 @@ export default function Home(): JSX.Element {
       img: 'img/IMG_0324.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: true,
     },
     {
       img: 'img/IMG_0610.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: false,
     },
     {
       img: 'img/IMG_0685.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: false,
     },
     {
       img: 'img/IMG_3165.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: true,
     },
     {
       img: 'img/IMG_3405.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: false,
     },
     {
       img: 'img/IMG_3959.jpeg',
       title: 'image1',
       author: 'author',
-      cols: 2,
+      cols: 1,
+      featured: false,
     },
   ];
 
@@ -50,10 +60,6 @@ export default function Home(): JSX.Element {
       <Head>
         <title>Ryohei KOMATSUYAMA</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
       </Head>
 
       <main className={styles.main}>
@@ -64,21 +70,32 @@ export default function Home(): JSX.Element {
           </p>
           <Icons></Icons>
         </div>
-
         <div>
           <p>1998年9月9日生まれ</p>
           <p>エンジニア</p>
         </div>
 
-        <div className={styles['grid-list-container']}>
-          <GridList cellHeight={160} className={styles['grid-list']} cols={3}>
+        <CssBaseline />
+        <Container
+          maxWidth="xs"
+          fixed
+          classes={{ maxWidthXs: '1000px' }}
+          className={styles['grid-list-container']}
+        >
+          {/* breakpointごとにグリッドの表示の比率を変える */}
+          <GridList cellHeight={200} spacing={1}>
             {tileData.map((tile) => (
-              <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <GridListTile
+                key={tile.img}
+                cols={tile.featured ? 2 : 1}
+                rows={tile.featured ? 2 : 1}
+              >
                 <img src={tile.img} alt={tile.title} />
+                <GridListTileBar title={tile.title} titlePosition="bottom" />
               </GridListTile>
             ))}
           </GridList>
-        </div>
+        </Container>
       </main>
     </div>
   );
